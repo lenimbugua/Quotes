f6a55696170ab1871d55043f3b331aa6fd25e02a
lenimbugua <leon9mbugua@gmail.com>
2018-03-12 16:38:54 +0300

    Bug fix: Proper fix

--- quotes/src/index.js
+++ quotes/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Jumbotron, Button, Container } from 'reactstrap';
 
 
-export default class Example extends React.Component {
+export default class QuoteNavbar extends React.Component {
   constructor(props) {
     super(props);
 
@@ -23,7 +23,7 @@ export default class Example extends React.Component {
   render() {
     return (
       <div>
-        <Navbar color="faded" light>
+        <Navbar color="purple" light>
           <NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse isOpen={!this.state.collapsed} navbar>
@@ -42,7 +42,43 @@ export default class Example extends React.Component {
   }
 }
 
+const Jumbotronquote = (props) => {
+  return (
+    <div>
+      <Jumbotron fluid>
+        <Container fluid>
+          <h3 className="display-3">Socrates</h3>
+          <p className="lead">
+            The truth is always more powerful than love,
+            because the love becomes powerless in the absence
+            of Truth
+          </p>
+          <h1 className="display-3">Lenimbugua</h1>
+          <p className="lead">
+            this is the new year the new you. You can pass through
+            another year, coasting on cruise control. Or you can step
+            out of your comfort zone, trying things you have never done before,
+            and make 2012 as the year that you elevate from where you are and soar
+            high. make it happen!!
+          </p>
+        </Container>
+      </Jumbotron>
+    </div>
+  );
+};
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <QuoteNavbar />,
+        <Jumbotronquote />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <Example />,
+  <App />,
   document.getElementById('root')
 );
